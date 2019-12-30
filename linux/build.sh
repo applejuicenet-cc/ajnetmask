ea17d07477e6044f9eae53bd5e666d02d3a1e02d
red171 <red171@appledocs.to>
2019-12-30 23:01:49 +0100

    add guide for windows

---
+++ linux/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "build libajnetmask for ${ARCH}"
+
+cd /src/
+
+  gcc -shared \
+    -I/usr/lib/jvm/java-8-openjdk-${ARCH}/include \
+    -I/usr/lib/jvm/java-8-openjdk-${ARCH}/include/linux \
+    -o /output/libajnetmask_${ARCH}.so de_applejuicenet_nativeclasses_JNISubNetMask.c
+
+file /output/libajnetmask_${ARCH}.so
