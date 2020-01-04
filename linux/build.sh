a6d7347ddbff639213a171d6e201bd6da3fdf5c4
red171 <red171@applejuicenet.cc>
2020-01-04 21:51:18 +0100

    fix linux build guide

--- linux/build.sh
+++ linux/build.sh
@@ -4,11 +4,9 @@ set -e
 
 echo "build libajnetmask for ${ARCH}"
 
-cd /src/
-
-  gcc -shared \
-    -I/usr/lib/jvm/java-8-openjdk-${ARCH}/include \
-    -I/usr/lib/jvm/java-8-openjdk-${ARCH}/include/linux \
-    -o /output/libajnetmask_${ARCH}.so de_applejuicenet_nativeclasses_JNISubNetMask.c
+gcc -shared \
+  -I/usr/lib/jvm/java-8-openjdk-${ARCH}/include \
+  -I/usr/lib/jvm/java-8-openjdk-${ARCH}/include/linux \
+  -o /output/libajnetmask_${ARCH}.so de_applejuicenet_nativeclasses_JNISubNetMask.c
 
 file /output/libajnetmask_${ARCH}.so
