c4a17635f169f033b7c56a0ff7ca8c815b009b28
red171 <red171@appledocs.to>
2020-04-21 11:09:15 +0200

    add latest build changes

--- windows/build.sh
+++ windows/build.sh
@@ -5,18 +5,26 @@ set -e
 echo "build ajnetmask.dll for windows ${ARCH}"
 
 if [ "${ARCH}" = "win86" ]; then
-  i686-w64-mingw32-g++ -shared -s \
+  i686-w64-mingw32-g++ -shared -Os -s \
+    -static-libgcc \
+    -static-libstdc++ \
     -I/tmp/jdk/ \
     -o /output/ajnetmask-i386.dll de_applejuicenet_nativeclasses_JNISubNetMask.cpp -lws2_32
 
   file /output/ajnetmask-i386.dll
 
+  ls -lah /output/ajnetmask-i386.dll
+
 else
 
-  x86_64-w64-mingw32-g++ -shared -s \
+  x86_64-w64-mingw32-g++ -shared -Os -s \
+    -static-libgcc \
+    -static-libstdc++ \
     -I/tmp/jdk/ \
     -o /output/ajnetmask-amd64.dll de_applejuicenet_nativeclasses_JNISubNetMask.cpp -lws2_32
 
   file /output/ajnetmask-amd64.dll
+
+  ls -lah /output/ajnetmask-amd64.dll
 
 fi
